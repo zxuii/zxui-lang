2cf3d76033e20d6717fae4290504f8223f20b1f2
zxuii <starssxproject@gmail.com>
2026-07-05 15:43:40 +0700

    tambahin builtin readline

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,5 +1,7 @@
 use crate::object::Value;
 
+use std::io::{self, Write};
+
 pub fn native_println(args: Vec<Value>) -> Result<Value, String> {
     let _ = native_print(args);
     println!();
@@ -18,4 +20,15 @@ pub fn native_print(args: Vec<Value>) -> Result<Value, String> {
         }
     }
     Ok(Value::Null)
+}
+
+pub fn native_readline(args: Vec<Value>) -> Result<Value, String> {
+    // kita expect kalau args yang dibutuhkan sudah pasti didapatkan
+    let mut input = String::new();
+
+    print!("{}", args[0]);
+    io::stdout().flush().expect("Failed to flush stdout");
+    io::stdin().read_line(&mut input).expect("Failed to readline");
+
+    Ok(Value::String(input))
 }
