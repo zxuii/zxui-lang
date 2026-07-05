0f03c1e45c772b2bfd030b39816bac74674540bc
zxuii <starssxproject@gmail.com>
2026-07-05 13:05:25 +0700

    buat interpreter ( untuk sekarang tak bisa call dan decl function dulu )

---
+++ src/interpreter.rs
@@ -0,0 +1,113 @@
+use crate::ast::{BinOp, Expr, Stmt, UnaryOp};
+use crate::environment::Environment;
+use crate::object::Object;
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+pub struct Interpreter {
+    env: Rc<RefCell<Environment>>
+}
+
+impl Interpreter {
+    pub fn new() -> Self {
+        Self {
+            env: Rc::new(RefCell::new(Environment::new()))
+        }
+    }
+
+    pub fn eval_expr(&self, expr: &Expr) -> Result<Object, String> {
+        match expr {
+            Expr::Number(num) => Ok(Object::Number(*num)),
+            Expr::Null => Ok(Object::Null),
+            Expr::NoOp => Ok(Object::Null),
+            Expr::Identifier(name) => {
+                self.env.borrow().get(name.clone())
+            }
+            Expr::Unary { op, expr } => {
+                let val = self.eval_expr(expr)?;
+                match (op, val) {
+                    (UnaryOp::Plus, Object::Number(num)) => Ok(Object::Number(num)),
+                    (UnaryOp::Minus, Object::Number(num)) => Ok(Object::Number(-num)),
+                    _ => Err("Unary op on non-number".into()),   
+                }
+            }
+            Expr::BinOp { left, op, right } => {
+                let l = self.eval_expr(left)?;
+                let r = self.eval_expr(right)?;
+                match (l, r) {
+                    (Object::Number(a), Object::Number(b)) => {
+                        let result = match op {
+                            BinOp::Plus => a + b,
+                            BinOp::Minus => a - b,
+                            BinOp::Multiply => a * b,
+                            BinOp::Divide => {
+                                if b == 0.0 { return Err("Division by zero".into())}
+                                a / b
+                            },
+                        };
+                        Ok(Object::Number(result))
+                    }
+                    _ => Err("Binary operation on non-numbers".into())
+                }
+            }
+
+            Expr::Call { callee, args} => {
+                todo!()
+            }
+        }
+    }
+
+    pub fn exec_stmt(&mut self, stmt: &Stmt) -> Result<Option<Object>, String> {
+        match stmt {
+            Stmt::Program(stmts) => {
+                let mut ret = None;
+                for stmt in stmts {
+                    ret = self.exec_stmt(stmt)?;
+                    if ret.is_some() { break; }
+                }
+                Ok(ret)
+            }
+            
+            Stmt::Block(stmts) => {
+                let child = Environment::new_enclosing(Some(Rc::clone(&self.env)));
+                let prev = Rc::clone(&self.env);
+                self.env = Rc::new(RefCell::new(child));
+
+                let mut ret = None;
+                for stmt in stmts {
+                    ret = self.exec_stmt(stmt)?;
+                    if ret.is_some() { break; }
+                }
+
+                self.env = prev;
+                Ok(ret)
+            }
+
+            Stmt::Let { name, expr } => {
+                let val = self.eval_expr(expr)?;
+                self.env.borrow_mut().define(name.clone(), val);
+                Ok(None)
+            }
+
+            Stmt::Assign { name, expr } => {
+                let val = self.eval_expr(expr)?;
+                self.env.borrow_mut().assign(name.clone(), val);
+                Ok(None)
+            }
+
+            Stmt::Return(expr) => {
+                let val = self.eval_expr(expr)?;
+                Ok(Some(val))
+            }
+
+            Stmt::ExprStmt(expr) => {
+                self.eval_expr(expr)?;
+                Ok(None)
+            }
+
+            Stmt::FunDecl { name, params, body } => todo!()
+
+        }
+    }
+}
