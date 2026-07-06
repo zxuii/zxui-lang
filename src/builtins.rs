24b0d1b33bbcad3b900664cea63f4f9eb74f28b5
zxuii <starssxproject@gmail.com>
2026-07-06 07:54:41 +0700

    simplifikasi untuk builtin print

--- src/builtins.rs
+++ src/builtins.rs
@@ -10,14 +10,7 @@ pub fn native_println(args: Vec<Value>) -> Result<Value, String> {
 
 pub fn native_print(args: Vec<Value>) -> Result<Value, String> {
     for arg in &args {
-        match arg {
-            Value::Number(n) => {
-                if n % 1.0 == 0.0 { print!("{}", *n as i64); }
-                else { print!("{}", n); }
-            }
-            Value::Null => print!("null"),
-            _ => print!("{:?}", arg),
-        }
+        print!("{}", arg);
     }
     Ok(Value::Null)
 }
