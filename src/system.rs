8fa5ce02745d4ab18eed34e2ba940da9a0e6c564
zxuii <starssxproject@gmail.com>
2026-07-22 18:34:08 +0700

    tambah system.exit()

--- src/system.rs
+++ src/system.rs
@@ -7,6 +7,7 @@ use crate::object::{NativeData, Value};
 pub fn make_system_module() -> IndexMap<String, Value> {
     let mut map = IndexMap::new();
     map.insert("os".to_string(), system_os());
+    map.insert("exit".to_string(), system_exit());
 
     map
 }
@@ -23,5 +24,18 @@ fn system_os() -> Value {
             #[cfg(target_os = "windows")]
             return Ok(Value::String("windows".to_string()));
         }),
+    ))
+}
+
+fn system_exit() -> Value {
+    Value::NativeFunction(NativeData::new(
+        "exit".to_string(),
+        1,
+        Rc::new(move |args| {
+            std::process::exit(match &args[0] {
+                Value::Number(n) => *n as i32,
+                other => return Err(format!("system.exit(): unsupported type '{}' expected type 'Number'", other).into())
+            })
+        })
     ))
 }
